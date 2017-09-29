9411b5cc72209122a7fedc8e8a93d9450f722c3d
user921 <krank2014@ya.ru>
2017-09-29 09:28:39 +0300

    change program version in cli.js

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@ import diff from './diff';
 
 const runConsoleProgram = () => {
   program
-    .version('0.5.0')
+    .version('0.6.0')
     .arguments('<firstConfig> <secondConfig>')
     .description('Compares two configuration files and shows a difference.')
     .option('-f, --format [type]', 'output format')
